2c01c4eee69e46c47476ce5e4c9538173a0fe4e6
ecaratao <ecaratao@johnsonsberkshire.com>
2022-03-10 16:48:31 +0800

    moding layouts

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.users.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('pages.admin.dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
